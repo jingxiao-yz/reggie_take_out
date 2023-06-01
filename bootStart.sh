886cf57ca52c4ae265e1006442909c9f6c4c4db5
jingxiao_yz <2437194031@qq.com>
2023-06-01 22:31:39 +0800

    final

--- bootStart.sh
+++ bootStart.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 echo =================================
-echo  自动化部署脚本启动
+echo 自动化部署脚本启动
 echo =================================
 
 echo 停止原来运行中的工程
 APP_NAME=reggie
 
-tpid=`ps -ef|grep $APP_NAME|grep -v grep|grep -v kill|awk '{print $2}'`
+tpid=$(ps -ef | grep $APP_NAME | grep -v grep | grep -v kill | awk '{print $2}')
 if [ ${tpid} ]; then
     echo 'Stop Process...'
     kill -15 $tpid
 fi
 sleep 2
-tpid=`ps -ef|grep $APP_NAME|grep -v grep|grep -v kill|awk '{print $2}'`
+tpid=$(ps -ef | grep $APP_NAME | grep -v grep | grep -v kill | awk '{print $2}')
 if [ ${tpid} ]; then
     echo 'Kill Process!'
     kill -9 $tpid
@@ -31,9 +31,10 @@ git pull
 echo 代码拉取完成
 
 echo 开始打包
-output=`mvn clean install -T 1C -Dmaven.test.skip=true -Dmaven.compile.fork=true`
+output=$(mvn clean install -T 1C -Dmaven.test.skip=true -Dmaven.compile.fork=true)
 
 cd target
 echo 启动项目
-nohup java -jar reggie_take_out-1.0-SNAPSHOT.jar & > reggie_take_out.log &
+nohup java -jar reggie_take_out-1.0-SNAPSHOT.jar &
+>reggie_take_out.log &
 echo 项目启动完成
